f6121b6c7f69e86209429e9b0585ff79df0a3792
nonylene <nonylene@gmail.com>
2020-08-30 14:58:08 +0900

    adduser.sh

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # isucon-scripts
+
 isucon scripts for innocent team
+
+## Usage
+
+
+```
+./adduser.sh
+```
---
+++ adduser.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -ex
+
+function add_user {
+  user=$1
+  sudo useradd -m $1
+  # NOPASSWD
+  echo "$1 ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/users
+}
+
+function put_authorized_keys {
+  user=$1
+  github_user=$2
+  sudo -u $1 mkdir -p /home/$1/.ssh
+  sudo -u $1 chmod 700 /home/$1/.ssh
+  sudo -u $1 curl https://github.com/$2.keys -o /home/$1/.ssh/authorized_keys
+  sudo -u $1 chmod 600 /home/$1/.ssh/authorized_keys
+}
+
+add_user nonylene
+add_user utgw
+add_user wass80
+
+put_authorized_keys nonylene nonylene
+put_authorized_keys utgw utgwkk
+put_authorized_keys wass80 wass88
